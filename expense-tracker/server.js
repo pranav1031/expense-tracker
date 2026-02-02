05860f4377a0d25e638f9e53e5693c16fb97523b
pranav1031 <pranavsharma1489@gmail.com>
2026-02-02 18:40:22 +0530

    vercel fixes 5

--- expense-tracker/server.js
+++ expense-tracker/server.js
@@ -22,9 +22,9 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/transactions', transactions);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'dist')));
+  app.use(express.static(path.join(__dirname, 'dist')));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'dist', 'index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
